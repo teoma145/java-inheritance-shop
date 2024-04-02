d20d6324dfe7421db474a0c7fb85b568d84e641a
teoma145 <matteodark145@yahoo.it>
2024-04-02 17:59:40 +0200

    fix

--- src/org/inheritance/Cart.java
+++ src/org/inheritance/Cart.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Cart {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Prodotto> carrello = new ArrayList<>();
+
 
         System.out.println("Quanti prodotti vuoi creare?");
         int numeroProdotti = scanner.nextInt();
         scanner.nextLine();
-        for (int i = 0; i < numeroProdotti; i++) {
+        Prodotto[] cart = new Prodotto[numeroProdotti];
+        for (int i = 0; i < cart.length; i++) {
             System.out.println("Inserisci il tipo di prodotto (smartphone, televisore, cuffie):");
             String tipoProdotto = scanner.nextLine();
             System.out.println("Inserisci il nome del prodotto:");
@@ -28,7 +29,8 @@ public class Cart {
                     int imei = scanner.nextInt();
                     System.out.println("Inserisci la memoria dello smartphone:");
                     int memoria = scanner.nextInt();
-                    carrello.add(new Smartphone(nome, marca, prezzo, iva, imei, memoria));
+                    scanner.nextLine();
+                    cart[i]= (new Smartphone(nome, marca, prezzo, iva, imei, memoria));
                     break;
                 case "televisore":
                     System.out.println("Inserisci la dimensione del televisore:");
@@ -36,14 +38,14 @@ public class Cart {
                     System.out.println("Il televisore è smart? (true/false):");
                     boolean smart = scanner.nextBoolean();
                     scanner.nextLine();
-                    carrello.add(new Televisori(nome, marca, prezzo, iva, dimensione, smart));
+                    cart[i]=(new Televisori(nome, marca, prezzo, iva, dimensione, smart));
                     break;
                 case "cuffie":
                     System.out.println("Inserisci il colore delle cuffie:");
                     String colore = scanner.nextLine();
                     System.out.println("Inserisci il tipo di connessione delle cuffie:");
                     String tipoConnessione = scanner.nextLine();
-                    carrello.add(new Cuffie(nome, marca, prezzo, iva, colore, tipoConnessione));
+                    cart[i]=(new Cuffie(nome, marca, prezzo, iva, colore, tipoConnessione));
                     break;
                 default:
                     System.out.println("Tipo di prodotto non riconosciuto.");
@@ -52,7 +54,7 @@ public class Cart {
         }
         BigDecimal prezzoTotale = BigDecimal.ZERO;
         System.out.println("Sintesi del carrello:");
-        for (Prodotto prodotto : carrello) {
+        for (Prodotto prodotto : cart) {
             System.out.println(prodotto.getDettagliProdotto());
             System.out.println("----------------------------------");
             prezzoTotale = prezzoTotale.add(prodotto.getPrezzoConIva());
